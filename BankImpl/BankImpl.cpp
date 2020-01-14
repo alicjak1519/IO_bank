8d3949a2d181d73e77018780041905bf76502826
enzoLorenzo <szymonxlorenc@gmail.com>
2020-01-14 01:01:41 +0100

    Rozbudowa menu

--- BankImpl/BankImpl.cpp
+++ BankImpl/BankImpl.cpp
@@ -8,27 +8,147 @@ using namespace std;
 
 int wybor;
 void start();
+void menuGlowne();
 
-
-void logowanie() {
+void stanKonta() {
 	system("cls");
-	cout << "----MENU----" << endl;
+	cout << "----STANY KONT----" << endl;
 	cout << endl;
-	cout << "1. Przelew." << endl;
-	cout << "2. Saldo." << endl;
-	cout << "3. Wyloguj." << endl;
+
+
+
+	cout << endl;
+	cout << "1. Cofnij" << endl;
 	cout << endl;
 	cout << "Podaj numer komendy: ";
 	cin >> wybor;
 
 	switch (wybor) {
 	case 1:
-		cout << "przelew" << endl;
+		menuGlowne();
+	};
+};
+
+void przelewy() {
+	system("cls");
+	cout << "----PRZELEWY----" << endl;
+	cout << endl;
+	cout << "1. Wykonaj przelew" << endl;
+	cout << "2. Zaplanuj przelew" << endl;
+	cout << "3. Zaplanuj zlecenie stale" << endl;
+	cout << "4. Historia" << endl;
+	cout << "5. Cofnij" << endl;
+	cout << endl;
+	cout << "Podaj numer komendy: ";
+	cin >> wybor;
+
+	switch (wybor) {
+	case 1:
+
 		break;
 	case 2:
-		cout << "saldo" << endl;
+
 		break;
 	case 3:
+
+		break;
+	case 4:
+
+		break;
+	case 5:
+		menuGlowne();
+	};
+};
+
+void lokaty() {
+	system("cls");
+	cout << "----LOKATY----" << endl;
+	cout << endl;
+	cout << "1. Sprawdz stan lokaty" << endl;
+	cout << "2. Zaloz lokate" << endl;
+	cout << "3. Wplac na lokate" << endl;
+	cout << "4. Rozwiaz lokate" << endl;
+	cout << "5. Cofnij" << endl;
+	cout << endl;
+	cout << "Podaj numer komendy: ";
+	cin >> wybor;
+
+	switch (wybor) {
+	case 1:
+
+		break;
+	case 2:
+
+		break;
+	case 3:
+
+		break;
+	case 4:
+
+		break;
+	case 5:
+		menuGlowne();
+	};
+};
+
+void kredyty() {
+	system("cls");
+	cout << "----KREDYTY----" << endl;
+	cout << endl;
+	cout << "1. Pokaz kredyty" << endl;
+	cout << "2. Zaplac rate" << endl;
+	cout << "3. Otworz kredyt" << endl;
+	cout << "4. Zamknij kredyt" << endl;
+	cout << "5. Cofnij" << endl;
+	cout << endl;
+	cout << "Podaj numer komendy: ";
+	cin >> wybor;
+
+	switch (wybor) {
+	case 1:
+
+		break;
+	case 2:
+
+		break;
+	case 3:
+
+		break;
+	case 4:
+
+		break;
+	case 5:
+		menuGlowne();
+	};
+};
+
+void menuGlowne() {
+	system("cls");
+	cout << "----MENU----" << endl;
+	cout << endl;
+	cout << "1. Stan konta" << endl;
+	cout << "2. Przelewy" << endl;
+	cout << "3. Lokaty" << endl;
+	cout << "4. Kredyty" << endl;
+	cout << "5. Wyloguj" << endl;
+	cout << endl;
+	cout << "Podaj numer komendy: ";
+	cin >> wybor;
+
+	switch (wybor) {
+	case 1:
+		stanKonta();
+		break;
+	case 2:
+		przelewy();
+		break;
+	case 3:
+		lokaty();
+		break;
+	case 4:
+		kredyty();
+		break;
+	case 5:
 		start();
 	};
 }
@@ -50,7 +170,7 @@ void start() {
 	
 	switch (wybor) {
 	case 1:
-		logowanie();
+		menuGlowne();
 		break;
 	case 2:
 		cout << "kon" << endl;
--- BankImpl/bank.h
+++ BankImpl/bank.h
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 
 using namespace std;
@@ -16,30 +17,6 @@ public:
 	Data(int y, int m, int d, int h, int min, int s) : year(y), month(m), day(d), hour(h), minute(min), second(s) {};
 };
 
-class KontoBankowe {
-public:
-	long numerKonta;
-	double saldo;
-	string typKonta;
-	KontoBankowe() {};
-	void sprawdzSaldo() {};
-	void wp³ac() {};
-	void wyplac() {};
-
-};
-
-class Przelew {
-public:
-	double wysokoscPrzelewu;
-	Data data;
-	string odbiorca;
-	string tytul;
-	long numerKontaOdiorcy;
-	Przelew();
-	void wplacNaKonto(KontoBankowe konto) {};
-	void pobierzZKonta(KontoBankowe konto) {};
-	void sprawdzMozliwoscWykonania(KontoBankowe konto) {};
-};
 
 class ZlecenieStale {
 public:
@@ -76,6 +53,32 @@ public:
 	void zatwierdzKredyt() {};
 };
 
+class KontoBankowe {
+public:
+	long numerKonta;
+	double saldo;
+	string typKonta;
+	vector <Kredyt> kredyty;
+	vector <Lokata> lokaty;
+	KontoBankowe() {};
+	void sprawdzSaldo() {};
+	void wp³ac() {};
+	void wyplac() {};
+
+};
+
+class Przelew {
+public:
+	double wysokoscPrzelewu;
+	Data data;
+	string odbiorca;
+	string tytul;
+	long numerKontaOdiorcy;
+	Przelew();
+	void wplacNaKonto(KontoBankowe konto) {};
+	void pobierzZKonta(KontoBankowe konto) {};
+	void sprawdzMozliwoscWykonania(KontoBankowe konto) {};
+};
 
 class Osoba {
 public:
@@ -93,7 +96,7 @@ public:
 
 class U¿ytkownik : public Osoba {
 public:
-	KontoBankowe konto;
+	vector <KontoBankowe> konta;
 	U¿ytkownik();
 	void sprawdzStanKonta() {};
 	void wezKredyt() {};
